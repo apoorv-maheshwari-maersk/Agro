38d99cb2d508b4af155872ea3be95277d7fd5dac
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-05-28 21:14:56 +0530

    Trusted Tech

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Hero.css';
+import gif from '../../static/images/farmer.png';
+
+function Hero()
+{
+    return(
+        <section className="hero">
+            <div className="hero-text">
+                <h2>Trusted Tech is the Effortless Blockchain Supply Chain Platform</h2>
+                <p className="hero-para">Trusted Tech is the easiest and safest way to buy, sell medicines online.
+                   Discover new ways to monetize and scale your business online with Trusted Tech.</p>
+            </div>
+
+            <div className="hero-img">
+                <img src={gif} />
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
